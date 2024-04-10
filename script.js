3f710846a3c8d6b13e82b0f7c79c71a7bc3e4061
Carlos Eduardo <carlosrrea44@gmail.com>
2024-04-10 17:09:20 -0300

    layout final tweaks, js inciated

---
+++ script.js
@@ -0,0 +1,25 @@
+const game = {
+    currentMove: 'X'
+}
+
+function getField(fieldNumber) {
+    const $field = document.querySelector('.scenario-field-' + fieldNumber) //.scenary-field-0
+    return $field
+}
+
+function toggleCurrentMove() {
+    if (game.currentMove == 'X') {
+        game.currentMove = 'O'
+    } else if (game.currentMove == 'O') {
+        game.currentMove= 'X'
+    }
+}
+
+for (let i = 0; i < 9; i++) {
+    const $field = getField(i)
+
+    $field.addEventListener('click', function(){
+        $field.textContent = game.currentMove
+        toggleCurrentMove()
+    })
+}
